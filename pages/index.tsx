6ae38f41f78ed29dc105da0883eb84b285340dad
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-05-04 15:57:15 +0430

    first page fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,19 +10,14 @@ const Home = () => {
         <title>HaDi BiGdEli</title>
         <meta name="description" content="سایت هادی بیگدلی" />
       </Head>
-      <div className="absolute inset-0 flex items-center  justify-center">
-        <div className="space-y-32">
-          <div className="w-[600px] p-10 mx-auto space-y-6 ">
-            <h1 dir="rtl" className="text-4xl font-bold text-center ">
-              به سایت
-            </h1>
-            <h1 dir="rtl" className="text-4xl font-bold text-center ">
-              HaDi BiGdEli
-            </h1>
-            <h1 dir="rtl" className="text-4xl font-bold text-center ">
-              خوش آمدید
-            </h1>
+      <div className=" py-40">
+        <div className="text-center max-w-2xl mx-auto font-bold space-y-20 px-10 ">
+          <div className="text-4xl space-y-3">
+            <p>به سایت</p>
+            <p>Hadi BiGdEli</p>
+            <p>خوش آمدید</p>
           </div>
+
           <Button
             label="محاسبه شاخص توده بدنی"
             active
